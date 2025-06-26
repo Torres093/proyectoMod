02441c6a2bba065fbc482c28ea24580c3378f311
Torres093 <marcosalejandrot583@gmail.com>
2025-06-26 12:14:23 -0600

    commit

---
+++ js/admin.js
@@ -0,0 +1,9 @@
+//API 
+const API_URL = "https://retoolapi.dev/RcO6RU/data"
+
+//imagenBB
+const IMG_API_URL = "https://api.imgbb.com/1/upload?key=8c59c3ffef056cf3b1c4115911576c33"
+
+const form = document.getElementById('juegoForm'); 
+const tituloEL = document.getElementById('nombre'); 
+const precioEL = document
--- js/script.js
+++ js/script.js
@@ -1,1 +1,39 @@
-console.log("Hola Mundo")
+console.log("Hola Mundo");
+const API_URL = "https://retoolapi.dev/RcO6RU/data";
+const container = document.getElementById('cards-container');
+
+async function CargarJuegos() {
+    try {
+        const res = await fetch(API_URL); 
+        const data = await res.json(); 
+        CargarTarjetas(data); 
+    } catch (err) {
+        console.error('Error al cargar los datos', err);
+        container.innerHTML = '<p>Error al cargar los juegos</p>'; 
+    }
+}
+
+function CargarTarjetas(juegos) {
+    container.innerHTML = '';
+    if (juegos.length === 0) {
+        container.innerHTML = "<p>No hay juegos registrados</p>";
+        return; 
+    }
+    juegos.forEach(juego => {
+        container.innerHTML += `
+        <article style="background-color: #121212; color: white;">
+        <img src="${juego.imagen }" alt="${juego.titulo}" style="width: 25%; border-radius: 0.5rem;">
+        <h3>${juego.titulo}</h3>
+        <p><strong>${juego.precio}</strong></p>
+        <p>Género: ${juego.genero}</p>
+        <button class="contrast">Agregar al carrito</button>
+        </article>
+        `;
+    });
+}
+
+window.addEventListener('DOMContentLoaded', CargarJuegos);
+
+
+
+
